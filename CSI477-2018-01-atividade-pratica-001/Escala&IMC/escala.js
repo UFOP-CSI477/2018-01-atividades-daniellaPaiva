2bcc982ad5773d00318345ab007d5c7587fb10c0
Daniella Paiva <daniellaspaiva@gmail.com>
2018-05-03 19:59:29 -0300

    ATV1 - completa

--- CSI477-2018-01-atividade-pratica-001/Escala&IMC/escala.js
+++ CSI477-2018-01-atividade-pratica-001/Escala&IMC/escala.js
@@ -12,6 +12,27 @@ function escala_ritcher(amplitude, deltaT) {
 
   //Apresentar o resultado
   $("input[name='res']").val(escala);
+
+  if(escala < 3.5){
+    $("#efeito").val("Geralmente não sentido, mas gravado.");
+  }
+  if(escala >= 3.5 && escala <= 5.4){
+    $("#efeito").val("Às vezes sentido, mas raramente causa danos.");
+  }
+  if(escala >= 5.5 && escala <= 6.0){
+    $("#efeito").val("No máximo causa pequenos danos a prédios bem construídos, mas pode danificar seriamente casas mal construídas em regiões próximas.");
+  }
+  if(escala >= 6.1 && escala <= 6.9){
+    $("#efeito").val("Pode ser destrutivo em áreas em torno de até 100km do epicentro.");
+  }
+  if(escala >= 7.0 && escala <= 7.9){
+    $("#efeito").val("Grande terremoto. Pode causar sérios danos numa grande faixa.");
+    alert("Grande terremoto. Pode causar sérios danos numa grande faixa.");
+  }
+  if(escala > 7.9){
+    $("#efeito").val("Enorme terremoto. Pode causar graves danos em muitas áreas mesmo que estejam a centenas de quilômetros.");
+  }
+
   
   return true;
 }
--- CSI477-2018-01-atividade-pratica-001/Escala&IMC/imc.js
+++ CSI477-2018-01-atividade-pratica-001/Escala&IMC/imc.js
@@ -11,6 +11,30 @@ function imc(w, h) {
 	//Apresentar o resultado
     $("input[name='res']").val(imc);
   
+  if(imc < 18.5){
+    $("#efeito").val("SUBNUTRIÇÃO");
+  }
+  if(imc >= 18.5 && imc <= 24.9){
+    $("#efeito").val("PESO SAULDÁVEL");
+  }
+  if(imc > 24.9 && imc <= 29.9){
+    $("#efeito").val("SOBREPESO");
+  }
+  if(imc > 29.9 && imc <= 34.9){
+    $("#efeito").val("OBESIDADE GRAU 1");
+  }
+  if(imc > 34.9 && imc <= 39.9){
+    $("#efeito").val("OBESIDADE GRAU 2");
+  }
+  if(imc > 39.9){
+    $("#efeito").val("OBESIDADE GRAU 3");
+  }
+
+  var peso_ideal = 20 * (altura * altura);
+
+  $("#ideal").val(peso_ideal);
+
+
   return true;
 }
 
